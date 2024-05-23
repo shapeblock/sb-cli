51afc41f05b3ecfeb21e7579f4df409af69b6105
Achanandhi-M <achanandhi.m@gmail.com>
2024-05-23 17:41:55 +0530

    Deployment status issue-fixed

--- sb/cmd/deploy_status.go
+++ sb/cmd/deploy_status.go
@@ -1,42 +1,84 @@
 package cmd
+
 import (
-			"fmt"
-			"os"
-		
-			"github.com/jedib0t/go-pretty/v6/table"
-			"github.com/spf13/cobra"
-		)
-		
-		var deployStatusCmd = &cobra.Command{
-			Use:   "status",
-			Short: "List all Deployment Status",
-			Run:   deployStatus,
-		}
-		
-		func deployStatus(cmd *cobra.Command, args []string) {
-			//TODO: if project context is set, list all apps in project context.
-			apps, err := fetchApps()
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error fetching apps: %v\n", err)
-				return
-			}
-		
-			t := table.NewWriter()
-			t.SetOutputMirror(os.Stdout)
-			t.SetStyle(table.StyleLight)
-			t.AppendHeader(table.Row{"UUID", "App Name", "Status"})
-			for _, app := range apps {
-				t.AppendRow([]interface{}{app.UUID, app.Name, deployment.app.Status})
-				t.AppendSeparator()
-			}
-			t.AppendSeparator()
-			t.Render()
-			if err != nil {
-			fmt.Println("Unable to parse response")
-			}
-		}
-		
-		func init() {
-			deployCmd.AddCommand(deployStatusCmd)
-		}
-		
+    "fmt"
+    "os"
+    "net/http"
+    "encoding/json"
+
+    "github.com/jedib0t/go-pretty/v6/table"
+    "github.com/spf13/cobra"
+    "github.com/spf13/viper"
+)
+
+var deployStatusCmd = &cobra.Command{
+    Use:   "status",
+    Short: "List all Deployment Status",
+    Run:   deployStatus,
+}
+
+
+type Deployment struct {
+    UUID   string `json:"uuid"`
+    Status string `json:"status"`
+}
+
+func deployStatus(cmd *cobra.Command, args []string) {
+    apps, err := fetchApps()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error fetching apps: %v\n", err)
+        return
+    }
+
+    sbUrl := viper.GetString("endpoint")
+    if sbUrl == "" {
+        fmt.Println("User not logged in")
+        return
+    }
+
+    token := viper.GetString("token")
+    if token == "" {
+        fmt.Println("User not logged in")
+        return
+    }
+
+    t := table.NewWriter()
+    t.SetOutputMirror(os.Stdout)
+    t.SetStyle(table.StyleLight)
+    t.AppendHeader(table.Row{"App Name", "App UUID", "Status"})
+
+    for _, app := range apps {
+        fullUrl := fmt.Sprintf("%s/api/apps/%s/deployments/", sbUrl, app.UUID)
+        req, err := http.NewRequest("GET", fullUrl, nil)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        req.Header.Add("Authorization", fmt.Sprintf("Token %s", token))
+
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        if err != nil {
+            fmt.Println("Error sending request:", err)
+            return
+        }
+        defer resp.Body.Close()
+
+        var deployments []Deployment
+        if err := json.NewDecoder(resp.Body).Decode(&deployments); err != nil {
+            fmt.Println("Error decoding JSON:", err)
+            return
+        }
+
+        for _, deployment := range deployments {
+            t.AppendRow([]interface{}{app.Name, app.UUID, deployment.Status})
+        }
+    }
+
+    t.Render()
+}
+
+func init() {
+    deployCmd.AddCommand(deployStatusCmd)
+}
